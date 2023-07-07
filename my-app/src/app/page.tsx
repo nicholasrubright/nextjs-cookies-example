e8f289a2f27708ee977e8da7886abce2268d53f9
DESKTOP-GRUEFOB\koalr <koalrube@gmail.com>
2023-07-07 15:02:42 -0400

    added stuff

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+export default async function Page() {
+  const response = await setCount();
+
+  const data = await response.json();
+
   return (
     <div>
       <h1>Hello world!</h1>
+      <p>Response: {data.count}</p>
     </div>
   );
+}
+
+async function setCount() {
+  return await fetch("http://localhost:3000/api", {
+    method: "POST",
+    cache: "no-cache",
+    credentials: "include",
+  });
 }
